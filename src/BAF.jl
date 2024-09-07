0902dfbf5e280363e85bcd343e8bf016755e5cd4
kaptsanovb <kaptsanovb@gmail.com>
2024-09-07 21:28:49 +0300

    Progress on BAF.jl: baf_header function

--- src/BAF.jl
+++ src/BAF.jl
@@ -1,5 +1,21 @@
 module BAF
 
-# Write your package code here.
+    @enum Endian begin
+        Small
+        Big
+    end
+
+    @enum Major begin
+        Row
+        Column
+    end
+
+
+    function baf_header(dims::Tuple{Vararg{Core.Integer}}, data_struct::T; endian::Endian = Endian.Little, major::Major = Major.Column)::Vector{UInt8} where T <: DataType
+        rank::UInt8 = length(dims)
+        settings::UInt8 = (major << 1) + endian
+
+        dims_bytecount::Tuple{Vararg{UInt8}} = ceil(ndigits.(dims, base=2) / 8)
+    end
 
 end
